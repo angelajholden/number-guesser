c415c97e29f2b1aa8fb65905c51a0e768b96e447
Angela J. Holden <angelajholden@gmail.com>
2019-09-27 08:50:45 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+	randomNumber = Math.floor(Math.random() * 9);
+	return randomNumber;
+}
+//generateTarget();
+
+const compareGuesses = (user, computer, target) => {
+	userDistance = Math.abs(user, target);
+	computerDistance = Math.abs(computer, target);
+	if (userDistance > computerDistance) {
+		return false;
+	} else if (computerDistance > userDistance) {
+		return true;
+	} else if (userDistance === computerDistance) {
+		return true;
+	}
+}
+
